a5b27fd828853e2ee40523f25938f45e53b5e788
Patryk Orłowski <orlowski.dev@gmail.com>
2023-06-18 10:40:55 +0200

    01 - Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+export default function App() {
+  return <div className="main-container">
+    <h1>Frontend Mentor - Newsletter sign-up form with success message solution</h1>
+  </div>
+}
